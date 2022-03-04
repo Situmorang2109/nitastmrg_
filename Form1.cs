cd1ffc7acfc9dd5f06d8fd467e64a532cce2be00
Anita Situmorang <93483591+Situmorang2109@users.noreply.github.com>
2022-03-04 18:08:15 +0700

    TP KPL

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Anita
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String nama = textBox1.Text;
+            MessageBox.Show("Hai " + nama + "\n You are a Strong Woman " + nama + " and Your Self <3 ");
+        }
+    }
+}
